4bded3c4b62259d70a4269c619a077b49848d390
GrahamDykes <153469002+GrahamDykes@users.noreply.github.com>
2024-09-11 23:44:51 -0700

    mongo failed

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ app.post('/todos', async (req, res) => {
 app.use(cors());
 app.use(express.json());
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost/mern-stack-db', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(`mongodb+srv://grahamdykes:<lQvKJDBj7YQviHmZ>@skeleton.uqb84.mongodb.net/`, { useNewUrlParser: true, useUnifiedTopology: true });
 // Define routes and middleware
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
