c0807ea4866e05adaff9fba425d0d0a1d97748dd
GrahamDykes <153469002+GrahamDykes@users.noreply.github.com>
2024-09-19 11:24:44 -0700

    CRUD functionality working!

--- backend/server.js
+++ backend/server.js
@@ -1,66 +1,94 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
 app.use(cors());
 
-
 //this cors setup should work in future projects
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
-var bodyParser = require('body-parser');
+// needed this for axios to send, and server to see req.body
+var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 const todoSchema = new mongoose.Schema({
   task: String,
   completed: Boolean,
 });
-const Todo = mongoose.model('Todo', todoSchema);
+const Todo = mongoose.model("Todo", todoSchema);
 
-app.get('/todos', async (req, res) => {
+app.get("/todos", async (req, res) => {
   const todos = await Todo.find();
   res.json(todos);
 });
 
 // Create a new todo
-app.post('/todos', async (req, res) => {
-  console.log('req.body:\n', req.body)
-    const newTodo = new Todo( {task: req.body.task, completed:false} );
-    await newTodo.save();
-    console.log('mongo:\n', newTodo)
-    console.log('_id:\n', newTodo._id)
-    res.json(newTodo);
-  });
-  // Update an existing todo
-  app.put('/todos/:id', async (req, res) => {
-    const updatedTodo = await Todo.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    res.json(updatedTodo);
-  });
-  // Delete a todo
-  app.delete('/todos/:id', async (req, res) => {
-    console.log('\n\nDelete Recieved:\n', req.params)
-    await Todo.findByIdAndDelete(req.params.id);
-    res.json({ message: 'Todo deleted successfully' });
-  });
+app.post("/todos", async (req, res) => {
+  console.log("req.body:\n", req.body);
+  const newTodo = new Todo({ task: req.body.task, completed: false });
+  await newTodo.save();
+  console.log("mongo:\n", newTodo);
+  console.log("_id:\n", newTodo._id);
+  res.json(newTodo);
+});
 
+// Update an existing todo
+app.put("/todos/:id", async (req, res) => {
+  console.log("\n\nUpdate Recieved:\n", req.params);
+   Todo.findOne({ _id: req.params.id })
+    .then(function (todo) {
+      if (!todo) {
+        console.log("Todo not found");
+        return res.status(404).send("Todo not found");
+      }
+      console.log("todo:\n", todo);
 
+      todo.completed = !todo.completed;
+
+      return todo.save();
+    })
+    .then(function (updatedTodo) {
+      // After saving the updated todo, fetch the full list of todos
+      return Todo.find(); // This returns a promise that resolves to the list of all Todos
+    })
+    .then(function (allTodos) {
+      // Send the full list of todos back to the frontend
+      res.json({ message: "Update Successful", todos: allTodos });
+    })
+    .catch((err) => console.log(err));
+
+  // res.json(res.data);
+
+  // res.redirect("/todos");
+});
+
+// Delete a todo
+app.delete("/todos/:id", async (req, res) => {
+  console.log("\n\nDelete Recieved:\n", req.params);
+  await Todo.findByIdAndDelete(req.params.id);
+  res.json({ message: "Todo deleted successfully" });
+});
 
 app.use(express.json());
 // Connect to MongoDB
 
-
 // Define routes and middleware
 app.listen(PORT, () => {
-  mongoose.connect(`mongodb+srv://grahamdykes:lQvKJDBj7YQviHmZ@skeleton.uqb84.mongodb.net/`)
-  .then(()=>console.log('Connected to DataBase'))
-  .catch(()=>console.log('DataBase failure'))
+  mongoose
+    .connect(
+      `mongodb+srv://grahamdykes:lQvKJDBj7YQviHmZ@skeleton.uqb84.mongodb.net/`
+    )
+    .then(() => console.log("Connected to DataBase"))
+    .catch(() => console.log("DataBase failure"));
   console.log(`Server is running on port ${PORT}`);
 });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,50 +1,89 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import TodoForm from './TodoForm';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import TodoForm from "./TodoForm";
+import "./App.css";
 
 const App = () => {
-const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([]);
 
-const addTodo = (newTodo) => {
+  const addTodo = (newTodo) => {
     setTodos([...todos, newTodo]);
   };
-  
-  
+
+  // const onAdd = (updatedTodo) => {
+  //   setTodos((prevTodos) => 
+  //     prevTodos.map((todo) => 
+  //       todo._id === updatedTodo._id ? updatedTodo : todo
+  //     )
+  //   );
+  // };
+
   useEffect(() => {
     // Fetch data from the Express server
-    axios.get('http://localhost:5000/todos')
-      .then(response => setTodos(response.data))
-      .catch(error => console.error(error));
+    axios
+      .get("http://localhost:5000/todos")
+      .then((response) => setTodos(response.data))
+      .catch((error) => console.error(error));
   }, []);
 
   const deleteTodo = async (e) => {
-    let deleteTarget = e.target.value
-
+    let deleteTarget = e.target.value;
     try {
-      console.log('Frontend is sending:\n', e.target.value)
-      const response = await axios.delete(`http://localhost:5000/todos/${deleteTarget}`);
+      console.log("Frontend is sending:\n", e.target.value);
+      const response = await axios.delete(
+        `http://localhost:5000/todos/${deleteTarget}`
+      );
       // onAdd(response.data);
+      // setTask('');
+      console.log('delete await complete')
+      console.log('delete response:\n', response)
+    } catch (error) {
+      console.error(error);
+    }
+    setTodos((todos) => todos.filter((u) => u._id !== deleteTarget));
+  };
+
+  const updateTodo = async (e) => {
+    let updatingTarget = e.target.value;
+    // console.log("e.target:\n", e.target);
+    // console.log("e.target.value:\n", e.target.value);
+    try {
+      console.log("Frontend update sending:\n", e.target.value);
+      const response = await axios.put(
+        `http://localhost:5000/todos/${updatingTarget}`
+      );
+      console.log('update await complete')
+      console.log("update response:\n", response);
+      setTodos(response.data.todos)
       // setTask('');
     } catch (error) {
       console.error(error);
     }
-
-    setTodos(todos => todos.filter((u) => u._id !== deleteTarget));
   };
 
   return (
     <div>
       <h1>MERN Stack Todo App</h1>
       <TodoForm onAdd={addTodo} />
-      <ul className='todoul'>
+      <ul className="todoul">
         {todos.map((todo) => (
           <li key={todo._id}>
             {todo.task}
-            {<button onClick={deleteTodo} value={todo._id}>X</button>}
-            {todo.completed===false?<p>NotDone</p>:<p>Done</p>}
-            </li>
+            {
+              <button onClick={deleteTodo} value={todo._id}>
+                X
+              </button>
+            }
+            {todo.completed === false ? (
+              <button onClick={updateTodo} value={todo._id}>
+                NotDone
+              </button>
+            ) : (
+              <button onClick={updateTodo} value={todo._id}>
+                Done
+              </button>
+            )}
+          </li>
         ))}
       </ul>
     </div>
